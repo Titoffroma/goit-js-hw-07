086978a88a42833e6f791d96640097d3a74b72a1
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2020-10-05 00:33:15 +0300

    main-js-improvements

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 let task = Array.from(document.querySelectorAll("nav a"));
+let sectionRef = document.querySelectorAll("section");
 let taskItem = document.querySelectorAll(".task");
-task.map((el, index) => {
+task.map((el, index, arr) => {
   el.addEventListener("blur", () => {
     taskItem[index].style.transform = "translateX(100%)";
     setTimeout(
       () => (taskItem[index].style.transform = "translateX(50%)"),
       300
     );
+    for (let i = 0; i < arr.length; i++) {
+      sectionRef[i].classList.remove("apear");
+    }
+    sectionRef[index].classList.add("apear");
   });
 });
