2fb2468e1dfd554174113934bb490650dff65e3b
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2020-10-18 04:13:44 +0300

    sass and vendor added

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 let task = Array.from(document.querySelectorAll("nav a"));
+let taskList = document.querySelector(".tasks-list");
 let sectionRef = document.querySelectorAll("section");
 let taskItem = document.querySelectorAll(".task");
-task.map((el, index, arr) => {
-  el.addEventListener("blur", () => {
-    taskItem[index].style.transform = "translateX(100%)";
-    taskItem[index].style.opacity = "0";
-    setTimeout(() => {
-      taskItem[index].style.transform = "translateX(0%)";
-      taskItem[index].style.opacity = "1";
-    }, 300);
-    for (let i = 0; i < arr.length; i++) {
-      sectionRef[i].classList.remove("apear");
-    }
-    sectionRef[index].classList.add("apear");
-  });
+taskList.addEventListener("click", (event) => {
+  let idx = task.indexOf(event.target);
+  taskItem[idx].style.transform = "translateX(100%)";
+  taskItem[idx].style.opacity = "0";
+  setTimeout(() => {
+    taskItem[idx].style.transform = "translateX(0%)";
+    taskItem[idx].style.opacity = "1";
+  }, 300);
+  for (let i = 0; i < task.length; i++) {
+    sectionRef[i].classList.remove("apear");
+  }
+  sectionRef[idx].classList.add("apear");
 });
